0bc247e5da8d66310f1036d5eb2b250c4e86f38a
Shiying Wu <shiyingwu991230@gmail.com>
2023-03-05 20:11:48 -0500

    add sudo to make sure no permission issue

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
+python3 -m pip install --upgrade pip
 pip3 install virtualenv
-virtualenv venv
+sudo virtualenv venv
 source venv/bin/activate
-pip3 install django pillow djangorestframework djangorestframework-simplejwt
+sudo pip3 install django pillow djangorestframework djangorestframework-simplejwt
