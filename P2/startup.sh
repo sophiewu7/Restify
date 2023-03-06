fcf6c9f4150d8ff8f5d76abaa79432050426bba0
Shiying Wu <shiyingwu991230@gmail.com>
2023-03-06 11:34:14 -0500

    add migration

--- P2/startup.sh
+++ P2/startup.sh
@@ -3,3 +3,6 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip
 python3 -m pip install --upgrade pip
 pip install django pillow djangorestframework djangorestframework-simplejwt
+cd ./restify/restify
+python ./manage.py makemigrations
+python ./manage.py migrate
