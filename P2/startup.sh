10622db119a09af9efbc484fe5e21f6905143b1f
Shiying Wu <shiyingwu991230@gmail.com>
2023-03-06 01:07:02 -0500

    remove virtualenv

--- P2/startup.sh
+++ P2/startup.sh
@@ -2,7 +2,4 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
 python3 -m pip install --upgrade pip
-pip3 install virtualenv
-sudo virtualenv venv
-source venv/bin/activate
-sudo pip3 install django pillow djangorestframework djangorestframework-simplejwt
+pip install django pillow djangorestframework djangorestframework-simplejwt
