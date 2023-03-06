2d981c9d692a0bdb531de223f05f789d436e5e7a
wushiyin <sophiewu@Pro7>
2023-03-06 12:44:32 -0500

    fix virtualenv problem, issue was should not be in a mounted network drive, permission issue arise with mounted network drive

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
-python3 -m pip install --upgrade pip
-pip install django pillow djangorestframework djangorestframework-simplejwt
+pip install virtualenv
+python -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install "django>=4.0.0,<4.1.0"
+pip install djangorestframework 
+pip install djangorestframework-simplejwt
+pip install pillow
+pip install pyyaml
+pip install requests
+pip install django-cors-headers
 cd ./restify/restify
 python ./manage.py makemigrations
 python ./manage.py migrate
