33a2cab0de275d92c66f2059c5e20fd2c5c6f481
Shiying Wu <sophiewu@Pro7>
2023-03-07 16:50:30 -0500

    create api app for token authentication

--- P2/restify/restify/settings.py
+++ P2/restify/restify/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'accounts',
+    'comments',
+    'notifications',
+    'properties',
+    'reservations',
 ]
 
 MIDDLEWARE = [
