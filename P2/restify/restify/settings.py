83a683e88e3f378024fca0d62dc8e1a1a0e82f7c
Shiying Wu <sophiewu@Pro7>
2023-03-09 13:15:22 -0500

    pagnition support

--- P2/restify/restify/settings.py
+++ P2/restify/restify/settings.py
@@ -152,6 +152,11 @@ REST_FRAMEWORK = {
     ),
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
+
 from datetime import timedelta
 
 SIMPLE_JWT = {
