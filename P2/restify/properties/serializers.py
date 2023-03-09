3a8300387c83189a9c45e6dd2913e4ec208cc2fe
Shiying Wu <sophiewu@Pro7>
2023-03-09 17:43:22 -0500

    remove search

--- P2/restify/properties/serializers.py
+++ P2/restify/properties/serializers.py
@@ -1,7 +1,8 @@
 
-from .models import Property
-from rest_framework import serializers
 from .models import Property, Availability
+from rest_framework import serializers
+from datetime import date, datetime
+
 
 class PropertySerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -56,3 +57,32 @@ class AvailabilitySerializer(serializers.ModelSerializer):
             'price',
             'property_id'
         ]
+
+# class SearchSerializer(serializers.Serializer):
+#     location = serializers.CharField(max_length=128)
+#     check_in = serializers.DateField(format='%Y-%m-%d')
+#     check_out = serializers.DateField(format='%Y-%m-%d')
+#     guests = serializers.IntegerField(min_value=1, required=False)
+#     min_price = serializers.IntegerField(min_value=0, required=False)
+#     max_price = serializers.IntegerField(min_value=0, required=False)
+#     bedrooms = serializers.IntegerField(min_value=1, required=False)
+#     washrooms = serializers.IntegerField(min_value=1, required=False)
+
+#     def validate_check_in(self, value):
+#         today = date.today()
+#         if value < today:
+#             raise serializers.ValidationError("Check-in date cannot be in the past")
+#         return value
+
+#     def validate_check_out(self, value):
+#         today = date.today()
+#         if value < today:
+#             raise serializers.ValidationError("Check-out date cannot be in the past")
+#         if self.initial_data.get('check_in') and value <= datetime.strptime(self.initial_data['check_in'], "%Y-%m-%d").date():
+#             raise serializers.ValidationError("Check-out date must be later than check-in date")
+#         return value
+
+#     def validate_max_price(self, value):
+#         if 'min_price' in self.initial_data and value < self.initial_data['min_price']:
+#             raise serializers.ValidationError("Max price must be greater than min price")
+#         return value
