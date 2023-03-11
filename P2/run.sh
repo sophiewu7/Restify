1dd2458423de4fed18cf7c4adad1b16523e82214
Shiying Wu <sophiewu@Pro7>
2023-03-11 14:12:48 -0500

    small modification

--- P2/run.sh
+++ P2/run.sh
@@ -4,4 +4,6 @@ python3 ./manage.py flush
 python3 ./manage.py loaddata data.json
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
+python3 ./manage.py flush
+python3 ./manage.py loaddata data.json
 python3 ./manage.py runserver
