534ed4092479d959e522b133b856d4d0aca8f543
Shiying Wu <shiyingwu991230@gmail.com>
2023-03-30 21:37:35 -0400

    startup.sh

---
+++ P3/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# sudo apt-get update
+# sudo apt-get install -y python3 python3-pip
+pip3 install virtualenv
+python3 -m venv venv
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install "django>=4.0.0,<4.1.0"
+pip3 install djangorestframework 
+pip3 install djangorestframework-simplejwt
+pip3 install pillow
+pip3 install pyyaml
+pip3 install requests
+pip3 install django-cors-headers
+pip3 install exc
+
+npm install react
+npm install react-router-dom
