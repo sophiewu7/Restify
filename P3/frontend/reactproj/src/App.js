880d1f61490f5da193f16c4beff7b6af3d937de2
Shiying Wu <shiyingwu991230@gmail.com>
2023-04-13 11:26:39 -0400

    always not logged when start

--- P3/frontend/reactproj/src/App.js
+++ P3/frontend/reactproj/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,29 +11,43 @@ import Register from './accounts/Register';
 import UserProfile from './accounts/Profile';
 import Password from './accounts/Password';
 
+import Search from './search/SearchBar';
+import SearchResults from './search/SearchResults';
+
 import NotificationPage from './notifications/notifications';
 import CommentSection from './comments/comments';
 import UserComment from './comments/user_comments'
 import AddUserComment from './comments/add_user_comment';
 
 
-const App = () => (
-    <Router>
-        <Layout>
-            <Routes>
-                <Route exact path='/' element={<Home />} />
-                <Route exact path='/signup' element={<Register />} />
-                <Route exact path='/login' element={<Login />} />
-                <Route exact path='/profile' element={<UserProfile />} />
-                <Route exact path='/reset_password' element={<Password />} />
-                <Route exact path='/notifications' element={<NotificationPage />} />
-                <Route exact path='/comments' element={<CommentSection propertyId={1}/>}/>
-                <Route exact path='/user_comments' element={<UserComment userId={1}/>}/>
-                <Route exact path='/user_comments' element={<UserComment userId={1}/>}/>
-                <Route exact path='/add_user_comments' element={<AddUserComment userId={1}/>}/>
-            </Routes>
-        </Layout>
-    </Router>
-);
+const App = () => {
+    useEffect(() => {
+        const accessToken = localStorage.getItem("access_token");
+        if (accessToken) {
+            localStorage.removeItem("access_token");
+        }
+    }, []);
+
+    return (
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route exact path='/signup' element={<Register />} />
+                    <Route exact path='/login' element={<Login />} />
+                    <Route exact path='/profile' element={<UserProfile />} />
+                    <Route exact path='/reset_password' element={<Password />} />
+                    <Route exact path='/search' element={<Search />} />
+                    <Route exact path='/search_results' element={<SearchResults />} />
+                    <Route exact path='/notifications' element={<NotificationPage />} />
+                    <Route exact path='/comments' element={<CommentSection propertyId={1}/>}/>
+                    <Route exact path='/user_comments' element={<UserComment userId={1}/>}/>
+                    <Route exact path='/user_comments' element={<UserComment userId={1}/>}/>
+                    <Route exact path='/add_user_comments' element={<AddUserComment userId={1}/>}/>
+                </Routes>
+            </Layout>
+        </Router>
+    );
+};
 
 export default App;
