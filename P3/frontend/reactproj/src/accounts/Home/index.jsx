b52222a292530a8a37a501216a9f782db80c7e25
Shiying Wu <shiyingwu991230@gmail.com>
2023-04-13 19:44:22 -0400

    home

--- P3/frontend/reactproj/src/accounts/Home/index.jsx
+++ P3/frontend/reactproj/src/accounts/Home/index.jsx
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Logout from '../Logout';
+import React from 'react';
+import SearchBar from '../../search/SearchBar'
+import './style.css';
 
 function Home() {
     
     return (
-        <div style={{ marginTop: "56px" }}>
-            home
+        <div className="bg-image d-flex justify-content-center align-items-center">
+            <div className="container d-flex justify-content-center align-items-center flex-column">
+                <div className="d-flex text-align-center flex-column ">
+                <h1 className="text-white text-center font-weight-bold">Book unique homes, vacation rentals, and more on Restify</h1>
+                <SearchBar />
+                </div>
+            </div>
         </div>
     );
         
